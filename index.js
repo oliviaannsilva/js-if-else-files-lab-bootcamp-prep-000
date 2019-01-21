6dfbc2ab2b0e5cab4b6704552219f080d2e2b24f
Olivia Silva <olivia.silva@wework.com>
2019-01-21 03:07:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (name === "Susan"){
   console.log("Expected:Susan,Received:"+index.name)
 }
 
-if (index.height)===70{
+if (index.height)===70){
 
 // Don't worry about this
 module.exports = { name, height
