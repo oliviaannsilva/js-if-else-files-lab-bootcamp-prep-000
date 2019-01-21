cf3dfe2cca07cfb69692b68f0da091b2f2d1e722
Olivia Silva <olivia.silva@wework.com>
2019-01-21 03:16:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Susan"){
+if (name === "Susan") {
   console.log("The name is correct")
 }else{
   console.log("Expected:Susan,Received:"+index.name)
 }
 
-if (index.height===70){
+if (index.height===70) {
   console.log("The height is correct")
 } else {
   console.log("Expected:70,Received:" +index.height)
